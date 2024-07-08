d2c286f27685bda5faad637b70a5679b4af0c605
Anurag Sharma <71564387+anur4ag@users.noreply.github.com>
2024-07-08 11:35:23 +0530

    inital commit

---
+++ app/(admin)/blogs/page.js
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const AdminBlog = () => {
+  return (
+    <>
+      <Button>Admin Blog</Button>
+      <div>Admin Blog</div>
+    </>
+  );
+};
+
+export default AdminBlog;
